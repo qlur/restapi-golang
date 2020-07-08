ff87da52561b72ce7d6813f43e9a19b7ee7c3953
lymsl <saulamsal@gmail.com>
2020-07-08 02:06:46 -0500

    added some random endpoints for testing

--- main.go
+++ main.go
@@ -2,8 +2,31 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net/http"
 )
 
+func homePage(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Homepage Endpoint hit")
+}
+
+func citiesData(w http.ResponseWriter, r *http.Request) {
+	cities := make([]string, 3)
+	cities[0] = "Santa Monica"
+	cities[1] = "Venice"
+	cities[2] = "Los Angeles"
+
+	fmt.Fprintf(w, cities[1])
+
+}
+
+func handleRequests() {
+	http.HandleFunc("/", homePage)
+	http.HandleFunc("/Venice", citiesData)
+	log.Fatal(http.ListenAndServe(":8081", nil))
+
+}
+
 func main() {
-	fmt.Println("RESTful API on Golang")
+	handleRequests()
 }
